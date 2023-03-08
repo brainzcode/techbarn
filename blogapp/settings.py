3cc33d37a5b473b62ec3a18373e8b700c609e41f
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-03-08 10:42:12 +0100

    Meta tag updated

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -24,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = config(
-    'SECRET_KEY', 'xs(tm40fo!9ztui27cke6yhygsm#ibyviu9_xgrpyzf4_e63vl')
+    'SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['blog-env2.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
