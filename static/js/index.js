8611f2f6abe0c9601e156a2f085f3c1e6ac76afc
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-03-14 03:57:44 +0100

    markdown reset

--- static/js/index.js
+++ static/js/index.js
@@ -11,6 +11,42 @@ function showPage() {
   document.getElementsByTagName("BODY")[0].style.display = "block";
 }
 
+// function darkMode() {
+//   const element = document.body;
+//   element.classList.toggle("dark-mode");
+
+//   const navElement = document.getElementById('nav');
+//   navElement.classList.toggle('dark-mode');
+// }
+
+// if (localStorage.getItem('theme' === 'dark')) {
+//   setDarkMode();
+
+//   if (document.getElementById('checkbox').checked) {
+//     localStorage.setItem('checkbox', true)
+//   }
+// }
+
+// function setDarkMode() {
+//   let isDark = document.body.classList.toggle('dark-mode')
+//   let isDark1 = document.querySelector('.card').classList.toggle('dark-mode')
+//   // let isDark2 = document.querySelector('.text-muted').classList.toggle('dark-mode')
+//   // let isDark3 = document.querySelector('.text-dark').classList.toggle('dark-mode')
+
+
+
+
+
+//   if (isDark || isDark1) {
+//     setDarkMode.checked = true;
+//     localStorage.setItem('theme', 'dark');
+//     document.getElementById('checkbox').setAttribute('checked', 'checked');
+//   } else {
+//     setDarkMode.checked = true;
+//     localStorage.removeItem('theme', 'dark')
+//   }
+// }
+
 
 
 
